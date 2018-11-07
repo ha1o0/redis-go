2f877e2d97c907c66d179473efd821c16a539f92
dp0qb <516202795@qq.com>
2018-11-08 01:00:46 +0800

    feat(server): run the command by reflect

--- server.go
+++ server.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net"
-	"strings"
+    "fmt"
+    "log"
+    "net"
+    "reflect"
+    "strings"
 )
 
 var zeroParamCommands = []string{"PING", "EXIT"}
@@ -21,8 +22,15 @@ var commandsMap = map[string]string{
 	"SET": "*3",
 	"EXIT": "*1",
 }
+
+var commandReflect = map[string]interface{}{
+    "ping": ping,
+    "exit": exit,
+    "get": get,
+    "set": set,
+}
+
 type key interface{}
-type value interface{}
 var valueMap = make(map[key]string)
 
 func main() {
@@ -62,6 +70,9 @@ func handleStuff(conn net.Conn) {
 		reqArr := strings.Split(req, "\r\n")
 		reqArr = reqArr[0:len(reqArr)-1]
 		fmt.Println("receive the client message：", reqArr)
+		if reqArr[2] == "COMMAND" {
+            break
+        }
 		handleCommands(reqArr, conn)
 	}
 }
@@ -71,45 +82,78 @@ func handleCommands(reqArr []string, conn net.Conn) {
 	commandName := strings.ToUpper(reqArr[2])
 	_, ok := commandsMap[commandName]
 	if !ok {
-		handleCommandError(1200, commandName, conn)
+		handleCommandError(1000, commandName, conn)
 		return
 	}
 	if commandsMap[commandName] != paramNumber {
-		handleCommandError(1201, commandName, conn)
+		handleCommandError(1001, commandName, conn)
 		return
 	}
-	handleCommand(reqArr, commandName, conn)
+	//handleCommand(reqArr, commandName, conn)
+    Apply(commandReflect[strings.ToLower(commandName)], []interface{}{reqArr, conn})
 }
 
 // handle the right command from client
 func handleCommand(reqArr []string, commandName string, conn net.Conn) {
-    switch commandName {
-    case "PING":
-        conn.Write([]byte("+PONG\r\n"))
-    case "GET":
-        result, ok := valueMap[reqArr[4]]
-        if !ok {
-            conn.Write([]byte("+(nil)\r\n"))
-        } else {
-            conn.Write([]byte("+\"" + result + "\"\r\n"))
-        }
-    case "SET":
-        valueMap[reqArr[4]] = reqArr[6]
-        conn.Write([]byte("+OK\r\n"))
-    case "EXIT":
-        conn.Close()
-    default:
-        conn.Write([]byte("+OTHER COMMAND\r\n"))
+   switch commandName {
+   case "PING":
+       conn.Write([]byte("+PONG\r\n"))
+   case "GET":
+       result, ok := valueMap[reqArr[4]]
+       if !ok {
+           conn.Write([]byte("+(nil)\r\n"))
+       } else {
+           conn.Write([]byte("+\"" + result + "\"\r\n"))
+       }
+   case "SET":
+       valueMap[reqArr[4]] = reqArr[6]
+       conn.Write([]byte("+OK\r\n"))
+   case "EXIT":
+       conn.Close()
+   default:
+       conn.Write([]byte("+OTHER COMMAND\r\n"))
+   }
+   fmt.Println("this connect end")
+}
+
+func ping(reqArr []string, conn net.Conn) {
+    conn.Write([]byte("+PONG\r\n"))
+}
+
+func exit(reqArr []string, conn net.Conn) {
+    conn.Close()
+}
+
+func set(reqArr []string, conn net.Conn) {
+    valueMap[reqArr[4]] = reqArr[6]
+    conn.Write([]byte("+OK\r\n"))
+}
+
+func get(reqArr []string, conn net.Conn) {
+    result, ok := valueMap[reqArr[4]]
+    if !ok {
+        conn.Write([]byte("+(nil)\r\n"))
+    } else {
+        conn.Write([]byte("+\"" + result + "\"\r\n"))
     }
-    fmt.Println("this connect end")
+}
+
+func Apply(f interface{}, args []interface{})([]reflect.Value){
+    fun := reflect.ValueOf(f)
+    in := make([]reflect.Value, len(args))
+    for k, param := range args{
+        in[k] = reflect.ValueOf(param)
+    }
+    r := fun.Call(in)
+    return r
 }
 
 // handle the error of the command from client
 func handleCommandError(errorCode int, commandName string, conn net.Conn) {
 	switch errorCode {
-	case 1200:
+	case 1000:
 		conn.Write([]byte("+(error) ERR unknown command '" + commandName + "' \r\n"))
-	case 1201:
+	case 1001:
 		conn.Write([]byte("+(error) ERR wrong number of arguments for " + commandName + " command\r\n"))
 	default:
 
