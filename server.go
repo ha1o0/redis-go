391bbc34b339a103753c4996996fcd6b6214da75
weifengsmile <516202795@qq.com>
2019-05-30 00:06:28 +0800

    feat(server): add lindex,lrange,ltrim commands

--- server.go
+++ server.go
@@ -28,6 +28,9 @@ var commandsMap = map[string]string{
 	"RPOP": "2",
 	"LPOP": "2",
 	"LLEN": "2",
+	"LINDEX": "3",
+	"LRANGE": "4",
+	"LTRIM": "4",
 	"EXIT": "1",
 }
 
@@ -41,6 +44,9 @@ var commandReflect = map[string]interface{}{
     "rpop": rpop,
     "lpop": lpop,
     "llen": llen,
+    "lindex": lindex,
+    "lrange": lrange,
+    "ltrim": ltrim,
 	"exit": exit,
 }
 
@@ -48,6 +54,7 @@ var valueMap = make(map[string]interface{})
 
 func main() {
 	fmt.Println("Start the tcp socket")
+	fmt.Println(len("aaa"))
 	startTcpServer()
 }
 
@@ -98,17 +105,33 @@ func handleCommands(reqArr []string, conn net.Conn) {
 		handleCommandError(1000, commandName, conn)
 		return
 	}
-	paramNumber, _ := strconv.ParseInt(strings.TrimPrefix(paramNumberStr, "*"), 0, 64)
+	paramNumber, err := strconv.ParseInt(strings.TrimPrefix(paramNumberStr, "*"), 0, 64)
+	if err != nil {
+		/// to do
+		handleCommandError(0, commandName, conn)
+		return
+	}
 	paramRequireNumberStr := commandsMap[commandName]
 	if strings.Contains(paramRequireNumberStr, ">=") {
-		paramRequireNumber, _ := strconv.ParseInt(strings.TrimPrefix(paramRequireNumberStr, ">="), 0, 64)
+		paramRequireNumber, err := strconv.ParseInt(strings.TrimPrefix(paramRequireNumberStr, ">="), 0, 64)
+		if err != nil {
+			/// to do
+			handleCommandError(0, commandName, conn)
+			return
+		}
 		if paramNumber < paramRequireNumber {
 			handleCommandError(1001, commandName, conn)
 			return
 		}
 		Apply(commandReflect[strings.ToLower(commandName)], []interface{}{reqArr, conn, int(paramNumber)})
+
 	} else {
-		paramRequireNumber, _ := strconv.ParseInt(paramRequireNumberStr, 0, 64)
+		paramRequireNumber, err := strconv.ParseInt(paramRequireNumberStr, 0, 64)
+		if err != nil {
+			/// to do
+			handleCommandError(0, commandName, conn)
+			return
+		}
 		if paramRequireNumber != paramNumber {
 			handleCommandError(1001, commandName, conn)
 			return
@@ -118,7 +141,7 @@ func handleCommands(reqArr []string, conn net.Conn) {
 }
 
 func ping(reqArr []string, conn net.Conn) {
-    conn.Write([]byte("+PONG\r\n"))
+	response(conn, 2, "PONG")
 }
 
 func exit(reqArr []string, conn net.Conn) {
@@ -127,35 +150,35 @@ func exit(reqArr []string, conn net.Conn) {
 
 func set(reqArr []string, conn net.Conn) {
     valueMap[reqArr[4]] = reqArr[6]
-    conn.Write([]byte("+OK\r\n"))
+	response(conn, 2, "OK")
 }
 
 func get(reqArr []string, conn net.Conn) {
     result, ok := valueMap[reqArr[4]]
     if !ok {
-        conn.Write([]byte("+(nil)\r\n"))
+		response(conn, 2, "(nil)")
     } else {
-        conn.Write([]byte("+\"" + result.(string) + "\"\r\n"))
+		response(conn, 1, result.(string))
     }
 }
 
 func exists(reqArr []string, conn net.Conn) {
 	_, ok := valueMap[reqArr[4]]
-	result := "0"
+	result := 0
 	if ok {
-		result = "1"
+		result = 1
 	}
-	conn.Write([]byte(":" + result + "\r\n"))
+	response(conn, 0, result)
 }
 
 func del(reqArr []string, conn net.Conn) {
 	_, ok := valueMap[reqArr[4]]
-	result := "0"
+	result := 0
 	if ok {
-		result = "1"
+		result = 1
 		delete(valueMap, reqArr[4])
 	}
-	conn.Write([]byte(":" + result + "\r\n"))
+	response(conn, 0, result)
 }
 
 func rpush(reqArr []string, conn net.Conn, paramNumber int) {
@@ -166,7 +189,7 @@ func rpush(reqArr []string, conn net.Conn, paramNumber int) {
 			sliceTemp = append(sliceTemp.([]interface{}), reqArr[2 * i + 4])
 		}
 		valueMap[reqArr[4]] = sliceTemp
-		conn.Write([]byte(":" + strconv.Itoa(len(sliceTemp.([]interface{}))) + "\r\n"))
+		response(conn, 0, len(sliceTemp.([]interface{})))
 	} else {
 		newSliceTemp := []interface{}{}
 		for i := 1; i <= valueNumber; i++ {
@@ -175,7 +198,7 @@ func rpush(reqArr []string, conn net.Conn, paramNumber int) {
 			fmt.Println(newSliceTemp)
 		}
 		valueMap[reqArr[4]] = newSliceTemp
-		conn.Write([]byte(":" + strconv.Itoa(valueNumber) + "\r\n"))
+		response(conn, 0, valueNumber)
 	}
 }
 
@@ -189,9 +212,9 @@ func rpop(reqArr []string, conn net.Conn) {
 		} else {
 			valueMap[reqArr[4]] = sliceTemp.([]interface{})[:sliceLength-1]
 		}
-		conn.Write([]byte("+\"" + lastElement + "\"\r\n"))
+		response(conn, 1, lastElement)
 	} else {
-		conn.Write([]byte("+(nil)\r\n"))
+		response(conn, 2, "(nil)")
 	}
 }
 
@@ -205,9 +228,9 @@ func lpop(reqArr []string, conn net.Conn) {
 		} else {
 			valueMap[reqArr[4]] = sliceTemp.([]interface{})[1:sliceLength]
 		}
-		conn.Write([]byte("+\"" + firstElement + "\"\r\n"))
+		response(conn, 1, firstElement)
 	} else {
-		conn.Write([]byte("+(nil)\r\n"))
+		response(conn, 2, "(nil)")
 	}
 }
 
@@ -217,7 +240,122 @@ func llen(reqArr []string, conn net.Conn) {
 	if ok {
 		sliceLength = len(sliceTemp.([]interface{}))
 	}
-	conn.Write([]byte(":" + strconv.Itoa(sliceLength) + "\r\n"))
+	response(conn, 0, sliceLength)
+}
+
+func lindex(reqArr []string, conn net.Conn) {
+	index, err := strconv.ParseInt(reqArr[6], 0, 64)
+	if err != nil {
+		handleCommandError(1003, "lindex", conn)
+		return
+	}
+	sliceTemp, ok := valueMap[reqArr[4]]
+	if ok {
+		fmt.Println(reflect.TypeOf(sliceTemp).String())
+		// judge if target value is slice
+		if !strings.Contains(reflect.TypeOf(sliceTemp).String(), "[]") {
+			handleCommandError(1002, "lindex", conn)
+			return
+		}
+		positionIndex := int(index)
+		result := ""
+		targetValue := sliceTemp.([]interface{})
+		if positionIndex < 0 {
+			positionIndex += len(targetValue)
+		}
+		if positionIndex >= 0 && positionIndex < len(targetValue) {
+			result = targetValue[positionIndex].(string)
+			response(conn, 1, result)
+		} else {
+			response(conn, 2, "(nil)")
+		}
+	} else {
+		response(conn, 2, "(nil)")
+	}
+}
+
+func lrange(reqArr []string, conn net.Conn) {
+	startIndex, startErr := strconv.ParseInt(reqArr[6], 0, 64)
+	endIndex, endErr := strconv.ParseInt(reqArr[8], 0, 64)
+	if startErr != nil || endErr != nil {
+		handleCommandError(1003, "lrange", conn)
+		return
+	}
+	sliceTemp, ok := valueMap[reqArr[4]]
+	if ok {
+		fmt.Println(reflect.TypeOf(sliceTemp).String())
+		// judge if target value is slice
+		if !strings.Contains(reflect.TypeOf(sliceTemp).String(), "[]") {
+			handleCommandError(1002, "lrange", conn)
+			return
+		}
+		startPositionIndex := int(startIndex)
+		endPositionIndex := int(endIndex)
+		result := ""
+		targetSlice := sliceTemp.([]interface{})
+		targetSliceLength := len(targetSlice)
+		startPositionIndex, endPositionIndex = getPositiveIndex(startPositionIndex, endPositionIndex, targetSliceLength)
+		if startPositionIndex > endPositionIndex || endPositionIndex < 0 {
+			response(conn, 2, "(empty list or set)")
+			return
+		}
+		j := 1
+		for i := startPositionIndex; i <= endPositionIndex; i++ {
+			commonResult := strconv.Itoa(j) + ") \"" + targetSlice[i].(string) + "\""
+			if i == endPositionIndex {
+				result += commonResult
+			} else {
+				result += commonResult + "\n"
+			}
+			j++
+		}
+		response(conn, 2, result)
+	} else {
+		response(conn, 2, "(empty list or set)")
+	}
+}
+
+func ltrim(reqArr []string, conn net.Conn) {
+	startIndex, startErr := strconv.ParseInt(reqArr[6], 0, 64)
+	endIndex, endErr := strconv.ParseInt(reqArr[8], 0, 64)
+	if startErr != nil || endErr != nil {
+		handleCommandError(1003, "ltrim", conn)
+		return
+	}
+	sliceTemp, ok := valueMap[reqArr[4]]
+	if ok {
+		fmt.Println(reflect.TypeOf(sliceTemp).String())
+		// judge if target value is slice
+		if !strings.Contains(reflect.TypeOf(sliceTemp).String(), "[]") {
+			handleCommandError(1002, "ltrim", conn)
+			return
+		}
+		startPositionIndex := int(startIndex)
+		endPositionIndex := int(endIndex)
+		targetSlice := sliceTemp.([]interface{})
+		targetSliceLength := len(targetSlice)
+		startPositionIndex, endPositionIndex = getPositiveIndex(startPositionIndex, endPositionIndex, targetSliceLength)
+		positionSub := endPositionIndex - startPositionIndex
+		if positionSub < 0 || endPositionIndex < 0 {
+			response(conn, 2, "(empty list or set)")
+			return
+		}
+		valueMap[reqArr[4]] = targetSlice[startPositionIndex:endPositionIndex + 1]
+		response(conn, 2, "OK")
+		//if positionSub <= targetSliceLength / 2 {
+		//	newSlice := []interface{}
+		//	for i:= startPositionIndex; i<= endPositionIndex; i++ {
+		//		newSlice = append(newSlice, targetSlice[i])
+		//	}
+		//	valueMap[reqArr[4]] = newSlice
+		//	response(conn, 2, "OK")
+		//} else {
+		//
+		//}
+		//
+	} else {
+		response(conn, 2, "(empty list or set)")
+	}
 }
 
 func Apply(f interface{}, args []interface{})([]reflect.Value){
@@ -230,6 +368,20 @@ func Apply(f interface{}, args []interface{})([]reflect.Value){
     return r
 }
 
+func response(conn net.Conn, responseType int, message interface{}) {
+	switch responseType {
+	// number
+	case 0:
+		conn.Write([]byte(":" + strconv.Itoa(message.(int)) + "\r\n"))
+	// "string"
+	case 1:
+		conn.Write([]byte("+" + stringWithQuotation(message.(string)) + "\r\n"))
+	// string
+	case 2:
+		conn.Write([]byte("+" + message.(string) + "\r\n"))
+	}
+}
+
 // handle the error of the command from client
 func handleCommandError(errorCode int, commandName string, conn net.Conn) {
 	switch errorCode {
@@ -237,10 +389,36 @@ func handleCommandError(errorCode int, commandName string, conn net.Conn) {
 		conn.Write([]byte("+(error) ERR unknown command '" + commandName + "' \r\n"))
 	case 1001:
 		conn.Write([]byte("+(error) ERR wrong number of arguments for " + commandName + " command\r\n"))
-	default:
+	case 1002:
+		conn.Write([]byte("+(error) WRONGTYPE Operation against a key holding the wrong kind of value\r\n"))
+	case 1003:
+		conn.Write([]byte("+(error) ERR value is not an integer or out of range\r\n"))
+	case 1004:
 
+	default:
+		conn.Write([]byte("+(error) unknown error\r\n"))
 	}
 	fmt.Println("this connect end")
+}
+
+func getPositiveIndex(startPositionIndex int, endPositionIndex int, targetSliceLength int) (int, int) {
+	if startPositionIndex < 0 {
+		startPositionIndex += targetSliceLength
+		if startPositionIndex < 0 {
+			startPositionIndex = 0
+		}
+	}
+	if endPositionIndex < 0 {
+		endPositionIndex += targetSliceLength
+	}
+	if endPositionIndex >= targetSliceLength {
+		endPositionIndex = targetSliceLength - 1
+	}
+	return startPositionIndex, endPositionIndex
+}
+
+func stringWithQuotation(str string) string {
+	return "\"" + str + "\""
 }
 
 // Judge if the element exists in array
